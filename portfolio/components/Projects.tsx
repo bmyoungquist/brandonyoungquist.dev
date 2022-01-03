0386a58cced1fcb351e5c0e2da073b16560edc62
bmyoungquist <bmyoungquist@gmail.com>
2022-01-02 23:07:55 -0600

    Changed Projects component to pull content dynamically from a json file in the public folder

--- portfolio/components/Projects.tsx
+++ portfolio/components/Projects.tsx
@@ -1,9 +1,120 @@
+import { useEffect, useState } from 'react';
 import Section from './Section';
 
+interface IProject {
+	title: string;
+	technologies: string[];
+	description: string;
+	imageUrl: string;
+	githubUrl?: string;
+	demoUrl?: string;
+	index: number;
+}
+
 const Projects: React.FunctionComponent = () => {
+	const [projects, setProjects] = useState([] as IProject[]);
+
+	useEffect(() => {
+		fetch(
+			'../data/projects.json',
+
+			{
+				headers: {
+					'Content-Type': 'application/json',
+
+					Accept: 'application/json',
+				},
+			}
+		).then(async (response) => {
+			setProjects(
+				((await response.json()) as IProject[]).sort(sortByIndex)
+			);
+		});
+	}, []);
+
+	const sortByIndex = (a: IProject, b: IProject) => {
+		if (a.index > b.index) {
+			return 1;
+		} else if (a.index < b.index) {
+			return -1;
+		}
+		return 0;
+	};
+
 	return (
 		<Section id="projects" title="Projects">
-			<div className="card">
+			{projects.map((project) => {
+				let img = (
+					<img
+						className="card"
+						src={project.imageUrl}
+						alt={project.title}
+					/>
+				);
+
+				let content = (
+					<div className="content">
+						<h5 key={`contentTitle${project.index}`}>
+							{project.title}
+						</h5>
+						<div
+							key={`contentTechnologies${project.index}`}
+							className="technologies"
+						>
+							{project.technologies.map((technology) => (
+								<span
+									key={`technology${project.index}${technology}`}
+								>
+									{technology}
+								</span>
+							))}
+						</div>
+						<p key={`contentDescription${project.index}`}>
+							{project.description}
+						</p>
+						<div className="links">
+							{project.githubUrl ? (
+								<a href={project.githubUrl} target="_blank">
+									<i className="fab fa-github"></i>
+								</a>
+							) : (
+								''
+							)}
+							{project.demoUrl ? (
+								<a href={project.demoUrl} target="_blank">
+									<i className="fas fa-external-link-alt"></i>
+								</a>
+							) : (
+								''
+							)}
+						</div>
+					</div>
+				);
+
+				if (project.index % 2 === 0) {
+					return (
+						<div
+							className="card"
+							key={`projectContent${project.index}`}
+						>
+							{content}
+							{img}
+						</div>
+					);
+				} else {
+					return (
+						<div
+							className="card"
+							key={`projectContent${project.index}`}
+						>
+							{img}
+							{content}
+						</div>
+					);
+				}
+			})}
+
+			{/* <div className="card">
 				<img
 					className="card"
 					src="//unsplash.it/1920/1080"
@@ -22,7 +133,7 @@ const Projects: React.FunctionComponent = () => {
 					src="//unsplash.it/1920/1080"
 					alt="cardimg"
 				/>
-			</div>
+			</div> */}
 		</Section>
 	);
 };
