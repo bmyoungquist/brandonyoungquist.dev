7e8a7c54ea234bfd7b44d09b1d1b65bd78282950
brandon.youngquist <brandon@youngquist.dev>
2026-01-01 15:33:15 -0600

    more build issue fixes

--- app/page.tsx
+++ app/page.tsx
@@ -6,15 +6,15 @@ import { Suspense } from 'react'
 
 export default function Home () {
 	return (
-		<div className="flex lg:flex-row flex-col sm:gap-12 gap-4 justify-around p-4 obverflow-x-hidden">
-			<div className="flex flex-col gap-4 md:w-1/2 w-full">
+		<div className="flex lg:flex-row flex-col sm:gap-12 gap-4 justify-around lg:p-4 md:p-4 sm:p-4 p-2 obverflow-x-hidden">
+			<div className="flex flex-col gap-4 sm:w-1/2 w-full">
 				<span className="max-w-max text-4xl font-black uppercase pb-1">Posts</span>
 				<Suspense>
 					<PostList posts={Posts.filter(p => p.enabled).slice(0, 7)} />
 				</Suspense>
 			</div>
 			<div className='w-[1px] border-l border-solid'></div>
-			<div className="flex flex-col gap-4 md:w-1/2 w-full">
+			<div className="flex flex-col gap-4 sm:w-1/2 w-full">
 				<span className="max-w-max text-4xl font-black uppercase pb-1">Photos</span>
 				<PhotoGrid photos={Photos.filter(p => p.enabled).slice(0, 6)} titleSize='xl' subtitleSize='md' titleClassName='px-3 py-2' />
 			</div>
